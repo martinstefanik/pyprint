3c418bcc2db5c4dec7d9518b012dd6bfac3d0d0b
Martin Stefanik <stefanik.mar@gmail.com>
2021-02-26 19:53:22 +0100

    Ignore hidden files and directories by default

--- pyprint.py
+++ pyprint.py
@@ -30,14 +30,20 @@ __version__ = "1.0.0"
     help="Run without actually sending the files to print. This shows the list "
     "of files that would be printed with the current set of options.",
 )
+@click.option(
+    "-h",
+    "--include-hidden",
+    is_flag=True,
+    help="Include hidden files and files in a hidden subdirectories of DIR.",
+)
 @click.version_option(version=__version__, message="%(version)s")
-def main(dir, pattern, dry_run):
+def main(dir, pattern, dry_run, include_hidden):
     """Print all files located in DIR."""
     pattern = re.compile(pattern)
-    to_print = files_to_print(dir, pattern)
+    to_print = files_to_print(dir, pattern, include_hidden)
     if dry_run:
         print("The following files would be sent to print:")
-        print("  " + "\n".join(to_print))
+        print("  " + "\n  ".join(to_print))
     else:
         for f in to_print:
             subprocess.run(
@@ -59,20 +65,26 @@ def main(dir, pattern, dry_run):
             )
 
 
-def files_to_print(dir, pattern):
+def files_to_print(dir, pattern, include_hidden):
     """Get a list of files in a directory whose names conform to a pattern."""
     to_print = []
-    for f in files(dir):
+    for f in files(dir, include_hidden):
         if os.path.isfile(f) and pattern.search(f):
             to_print.append(f)
     return to_print
 
 
-def files(dir):
-    """Generate all files and directories located under a given directory."""
+def files(dir, include_hidden):
+    """Generate files and directories located under a given directory."""
     for root, dirs, files in os.walk(dir):
-        for f in dirs + files:
-            yield os.path.join(root, f)
+        if not include_hidden:
+            if re.search(r"\.(?!/|$).*", root) is not None:
+                continue
+            entries = [e for e in dirs + files if not e.startswith(".")]
+        else:
+            entries = dirs + files
+        for e in entries:
+            yield os.path.join(root, e)
 
 
 if __name__ == "__main__":
