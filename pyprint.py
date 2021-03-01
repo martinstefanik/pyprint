964dd48b6d5a74c297522b879d9098496ad18044
Martin Stefanik <stefanik.mar@gmail.com>
2021-03-01 19:58:34 +0100

    Automatically add the correct stapling option
    The name of the option for stapling may depend on the brand of the
    printer that is being used. With this change, the script attemps to set
    the stapling option correctly for the particular printer that the job is
    being sent to automatically.

--- pyprint.py
+++ pyprint.py
@@ -45,8 +45,14 @@ __version__ = "1.0.0"
     show_default=False,
     help="Python regular expression to filter out files to be printed.",
 )
+@click.option(
+    "-s",
+    "--staple",
+    is_flag=True,
+    help="Indicates whether pages of each printed document should be stapled.",
+)
 @click.version_option(version=__version__, message="%(version)s")
-def main(dir, dry_run, include_hidden, printer, regex):
+def main(dir, dry_run, include_hidden, printer, regex, staple):
     """
     Print all files located under DIR. Files without read permission and files
     in directories without read permission are automatically ignored.
@@ -74,34 +80,15 @@ def main(dir, dry_run, include_hidden, printer, regex):
     if len(to_print) == 0:
         raise click.ClickException(f"No files matching '{regex}' in '{dir}'.")
     if dry_run:
-        if len(to_print) == 0:
-            click.echo(f"No files matching '{regex}' in '{dir}'.")
-        else:
-            print(
-                f"The following files would be sent to printer '{printer}':\n"
-                "  " + "\n  ".join(to_print)
-            )
+        _ = build_print_command(printer, staple, to_print)
+        print(
+            f"The following files would be sent to printer '{printer}':\n"
+            "  " + "\n  ".join(to_print)
+        )
     else:
+        command = build_print_command(printer, staple, to_print)
         try:
-            subprocess.run(
-                [
-                    "lp",
-                    "-d",
-                    printer,
-                    "-o",
-                    "sides=two-sided-long-edge",
-                    "-o",
-                    "media=A4",
-                    "-o",
-                    "collate=true",
-                    "-o",
-                    "HPStaplerOptions=1StapleLeft",
-                    "--",
-                ]
-                + to_print,
-                stdout=subprocess.DEVNULL,
-                check=True,
-            )
+            subprocess.run(command, stdout=subprocess.DEVNULL, check=True)
         except Exception:
             click.echo("Printing failed")
 
@@ -142,6 +129,56 @@ def files(dir, include_hidden):
             entries = dirs + files
         for e in entries:
             yield os.path.join(root, e)
+
+
+def build_print_command(printer, staple, to_print):
+    """Build an `lp` print command based on the user input."""
+    stapling_option = set_stapling_option(printer, staple)
+    command = [
+        "lp",
+        "-d",
+        printer,
+        "-o",
+        "sides=two-sided-long-edge",
+        "-o",
+        "media=A4",
+        "-o",
+        "collate=true",
+    ]
+    if staple and stapling_option is not None:
+        command.extend(["-o", stapling_option])
+    command.extend(["--"])
+    command.extend(to_print)
+
+    return command
+
+
+def set_stapling_option(printer, staple):
+    """Get the printer-specific name of the option for staping and set it."""
+    pattern = re.compile(r"staple", flags=re.IGNORECASE)
+    pr = subprocess.run(
+        ["lpoptions", "-p", printer, "-l"], check=True, capture_output=True
+    )
+    options = pr.stdout.decode("UTF-8").splitlines()
+    stapling = [opt for opt in options if pattern.search(opt) is not None]
+
+    if not staple and not stapling:
+        return None
+    elif staple and not stapling:
+        raise click.ClickException(
+            f"Printer '{printer}' has no stapling functionality."
+        )
+    name = stapling[0].split("/")[0]  # stapling option name
+    values = stapling[0].split(":")[1]  # possible values for the option
+
+    # Set the stapling option to the desired value
+    if staple:  # `stapling` is True at this point
+        pattern = re.compile(r"(?<!\*)\S*left\S*", flags=re.IGNORECASE)
+        target_value = pattern.search(values).group(0)
+    else:
+        target_value = "None"
+
+    return f"{name}={target_value}"
 
 
 if __name__ == "__main__":
